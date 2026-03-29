c9b1d41b5c0800523daa43110e70e10a1e50955b
Paolo <paoriellecloud@gmail.com>
2026-03-28 20:09:27 -0400

    Fix bcrypt/passlib incompatibilité - version fixe

--- auth.py
+++ auth.py
@@ -13,7 +13,7 @@ SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
